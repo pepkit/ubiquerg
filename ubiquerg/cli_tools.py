d570037cb2d25aba35729ae67c5dbddaf4894034
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-05-30 12:06:50 -0400

    add query_yes_no

--- ubiquerg/cli_tools.py
+++ ubiquerg/cli_tools.py
@@ -1,11 +1,12 @@
 """ Functions for working with command-line interaction """
 
 from .collection import is_collection_like
+from sys.stdout import write
 
 __author__ = "Vince Reuter"
 __email__ = "vreuter@virginia.edu"
 
-__all__ = ["build_cli_extra"]
+__all__ = ["build_cli_extra", "query_yes_no"]
 
 
 def build_cli_extra(optargs):
@@ -39,3 +40,35 @@ def build_cli_extra(optargs):
         data_iter = optargs
 
     return " ".join(render(*kv) for kv in data_iter)
+
+
+def query_yes_no(question, default="no"):
+    """
+    Ask a yes/no question via raw_input() and return their answer.
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {
+        "yes": True, "y": True, "ye": True,
+        "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
