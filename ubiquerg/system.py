669952bd334edac5480039babf8a1fd9c748a2dc
nsheff <nsheff@users.noreply.github.com>
2019-06-14 14:48:17 -0400

    add is_command_callable

--- ubiquerg/system.py
+++ ubiquerg/system.py
@@ -8,7 +8,7 @@ __all__ = ["is_command_callable"]
 
 
 
-def is_command_callable(self, command, name=""):
+def is_command_callable(command):
     """
     Check if command can be called.
 
@@ -16,12 +16,8 @@ def is_command_callable(self, command, name=""):
     :param str name: nickname/alias by which to reference the command
     :return bool: whether given command's call succeeded
     """
-    name = name or command
     # Use `command` to see if command is callable, store exit code
     code = os.system(
         "command -v {0} >/dev/null 2>&1 || {{ exit 1; }}".format(command))
-    if code != 0:
-        alias_value = " ('{}') ".format(name) if name else " "
-        _LOGGER.debug("Command '{0}' is not callable: {1}".
-                      format(alias_value, command))
+
     return not bool(code)
