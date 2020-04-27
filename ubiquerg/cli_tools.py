b1dc6e19e440d2f3bef5ff2f1dc6623db3c4e967
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-04-27 17:41:36 -0400

    extend argumentparer class with dests retrieval methods
    related to https://github.com/pepkit/looper/issues/253

--- ubiquerg/cli_tools.py
+++ ubiquerg/cli_tools.py
@@ -1,11 +1,9 @@
 """ Functions for working with command-line interaction """
 
 from .collection import is_collection_like
-from argparse import ArgumentParser
+from argparse import ArgumentParser, _SubParsersAction, _HelpAction
 import sys
 
-__author__ = "Vince Reuter"
-__email__ = "vreuter@virginia.edu"
 __classes__ = ["VersionInHelpParser"]
 __all__ = __classes__ + ["build_cli_extra", "query_yes_no"]
 
@@ -13,16 +11,62 @@ __all__ = __classes__ + ["build_cli_extra", "query_yes_no"]
 class VersionInHelpParser(ArgumentParser):
 
     def __init__(self, version=None, **kwargs):
-        """ Overwrites the inherited init. Saves the version as an object attribute for further use. """
+        """ Overwrites the inherited init. Saves the version as an object
+        attribute for further use. """
         super(VersionInHelpParser, self).__init__(**kwargs)
         self.version = version
         if self.version is not None:
-            self.add_argument('--version', action='version', version='%(prog)s {}'.format(self.version))
+            self.add_argument('--version', action='version',
+                              version='%(prog)s {}'.format(self.version))
 
     def format_help(self):
         """ Add version information to help text. """
-        help_string = "version: {}\n".format(str(self.version)) if self.version is not None else ""
+        help_string = "version: {}\n".format(str(self.version)) \
+            if self.version is not None else ""
         return help_string + super(VersionInHelpParser, self).format_help()
+
+    def subparsers(self):
+        """
+        Get the subparser associated with a parser.
+
+        :return argparse._SubparsersAction: action defining the subparsers
+        """
+        subs = [a for a in self._actions if isinstance(a, _SubParsersAction)]
+        if len(subs) != 1:
+            raise ValueError(
+                "Expected exactly 1 subparser, got {}".format(len(subs)))
+        return subs[0]
+
+    def subcommands(self):
+        """
+        Get subcommands defined by a parser.
+
+        :return list[str]: subcommands defined within this parser
+        """
+        return list(self.subparsers().choices.keys())
+
+    def dests_by_subparser(self, subcommand=None):
+        """
+        Get argument dests by subcommand from a parser.
+
+        :param str subcommand: subcommand to get dests for
+        :return dict: dests by subcommand
+        """
+        if subcommand is not None and subcommand not in self.subcommands():
+            raise ValueError("'{}' not found in this parser commands: {}".
+                             format(subcommand, str(self.subcommands())))
+        subs = self.subparsers().choices if subcommand is None \
+            else {subcommand: self.subparsers().choices[subcommand]}
+        dests = {}
+        for subcmd, sub in subs.items():
+            dest_list = []
+            for action in sub._actions:
+                if isinstance(action, _HelpAction):
+                    continue
+                if hasattr(action, "dest"):
+                    dest_list.append(action.dest)
+            dests[subcmd] = dest_list
+        return dests
 
 
 def build_cli_extra(optargs):
@@ -67,7 +111,8 @@ def query_yes_no(question, default="no"):
     :return bool: True for "yes" or False for "no"
     """
     def parse(ans):
-        return {"yes": True, "y": True, "ye": True, "no": False, "n": False}[ans.lower()]
+        return {"yes": True, "y": True, "ye": True,
+                "no": False, "n": False}[ans.lower()]
     try:
         prompt = {None: "[y/n]", "yes": "[Y/n]",
                   "no": "[y/N]"}[None if default is None else default.lower()]
@@ -79,7 +124,8 @@ def query_yes_no(question, default="no"):
         try:
             return parse(_read_from_user() or default)
         except KeyError:
-            sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+            sys.stdout.write(
+                "Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
 
 
 def _read_from_user():
@@ -88,4 +134,3 @@ def _read_from_user():
         from __builtin__ import raw_input
         return raw_input()
     return input()
-
