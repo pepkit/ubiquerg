d1677d84dcf2f9a231c7fa0d282b673973819b80
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-06-19 11:18:18 -0400

    add deep_update to __all__

--- ubiquerg/collection.py
+++ ubiquerg/collection.py
@@ -13,7 +13,7 @@ __author__ = "Vince Reuter"
 __email__ = "vreuter@virginia.edu"
 
 
-__all__ = ["is_collection_like", "powerset", "asciify_dict", "merge_dicts"]
+__all__ = ["is_collection_like", "powerset", "asciify_dict", "merge_dicts", "deep_update"]
 
 
 def merge_dicts(x, y):
