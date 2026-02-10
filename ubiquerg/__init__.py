243423d729293045b746e8f30dfddefeccce45b3
nsheff <nsheff@users.noreply.github.com>
2026-02-10 08:37:49 -0500

    make exports explicit

--- ubiquerg/__init__.py
+++ ubiquerg/__init__.py
@@ -5,11 +5,71 @@ Version is defined in pyproject.toml. To get it at runtime:
     version("ubiquerg")
 """
 
-from .cli_tools import *
-from .collection import *
-from .environment import *
-from .file_locking import *
-from .files import *
-from .paths import *
-from .system import *
-from .web import *
+from .cli_tools import (
+    VersionInHelpParser,
+    build_cli_extra,
+    convert_value,
+    query_yes_no,
+)
+from .collection import deep_update, is_collection_like, merge_dicts, powerset
+from .environment import TmpEnv
+from .file_locking import (
+    ThreeLocker,
+    ensure_locked,
+    ensure_write_access,
+    locked_read_file,
+    make_all_lock_paths,
+    read_lock,
+    wait_for_locks,
+    write_lock,
+)
+from .files import (
+    checksum,
+    create_file_racefree,
+    create_lock,
+    filesize_to_str,
+    make_lock_path,
+    remove_lock,
+    size,
+    untar,
+    wait_for_lock,
+)
+from .paths import expandpath, mkabs, parse_registry_path, parse_registry_path_strict
+from .system import is_command_callable, is_writable
+from .web import is_url
+
+__all__ = [
+    "build_cli_extra",
+    "checksum",
+    "convert_value",
+    "create_file_racefree",
+    "create_lock",
+    "deep_update",
+    "ensure_locked",
+    "ensure_write_access",
+    "expandpath",
+    "filesize_to_str",
+    "is_collection_like",
+    "is_command_callable",
+    "is_url",
+    "is_writable",
+    "locked_read_file",
+    "make_all_lock_paths",
+    "make_lock_path",
+    "merge_dicts",
+    "mkabs",
+    "parse_registry_path",
+    "parse_registry_path_strict",
+    "powerset",
+    "query_yes_no",
+    "read_lock",
+    "remove_lock",
+    "size",
+    "ThreeLocker",
+    "TmpEnv",
+    "untar",
+    "VersionInHelpParser",
+    "wait_for_lock",
+    "wait_for_locks",
+    "write_lock",
+]
